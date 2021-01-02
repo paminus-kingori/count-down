b30e7bfb23b7a235cf3ae1c71ae40cf68f173d90
paminus-kingori <pkmurungi.pm@gmail.com>
2021-01-03 00:26:14 +0300

    done

---
+++ count.js
@@ -0,0 +1,28 @@
+let sec = document.querySelector(".sec");
+let min = document.querySelector(".min");
+let hrs = document.querySelector(".hrs");
+let days = document.querySelector(".days")
+
+
+// const removeTime = (x)=>{
+// return (parseInt(x)-1)
+// }
+
+// console.log(removeTime(sec.innerText))
+let bday = [21,0,59,59];
+
+
+const findDifference = ()=>{
+let today = new Date();
+let day = today.getDate();
+let todHrs = today.getHours();
+let todMin = today.getMinutes();   
+let todSec = today.getSeconds();
+
+    days.innerText =bday[0] - day 
+    hrs.innerText = todHrs - bday[1]
+    min.innerText = bday[2] - todMin 
+    sec.innerText = bday[3] - todSec
+}
+
+setInterval(findDifference,1000);
